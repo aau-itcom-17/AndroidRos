357b00e724d74e8b70b0fd43f24a70905fb6a0fe
Sucramo <31632099+Sucramo@users.noreply.github.com>
2018-04-06 12:41:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marcu/androidros/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.marcu.androidros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
