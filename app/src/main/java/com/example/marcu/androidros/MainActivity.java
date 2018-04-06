91d89091b948b4e1b1f660b42b0439fda6c0755f
bastianhoj <bastianhoejbjerre@gmail.com>
2018-04-06 14:18:39 +0200

    Bottom Navigation Bar

--- app/src/main/java/com/example/marcu/androidros/MainActivity.java
+++ app/src/main/java/com/example/marcu/androidros/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.marcu.androidros;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_map:
+                    mTextMessage.setText(R.string.title_map);
+                    return true;
+                case R.id.navigation_list:
+                    mTextMessage.setText(R.string.title_list);
+                    return true;
+                case R.id.navigation_favourites:
+                    mTextMessage.setText(R.string.title_favourites);
+                    return true;
+                case R.id.navigation_my_events:
+                    mTextMessage.setText(R.string.title_my_events);
+                    return true;
+
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
