575c9a6d7646aff4b7a0ab5bdddf2e0a8cef45ff
bastianhoj <bastianhoejbjerre@gmail.com>
2018-04-12 09:56:55 +0200

    Made a button on the list fragment. When this button is clicked it opens a pop up with an event

--- app/src/main/java/com/example/marcu/androidros/ListFragment.java
+++ app/src/main/java/com/example/marcu/androidros/ListFragment.java
@@ -1,17 +1,41 @@
 package com.example.marcu.androidros;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import com.example.marcu.androidros.Event;
 
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment implements View.OnClickListener {
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        Button b = (Button) view.findViewById(R.id.button);
+        b.setOnClickListener(this);
+        return view;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                startActivity(new Intent(getActivity(), Event.class));
+
+
+                break;
+        }
+    }
+
+
+
+
+
 }
