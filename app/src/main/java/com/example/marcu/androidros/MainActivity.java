f6af8beb24507c41c16b88e0bc5a19eba85a3a08
daugerdas <daugerdas@gmail.com>
2018-04-12 12:19:55 +0200

    Added additional permissions to Facebook login (public_profile, EMAIL, user_photos, user_friends)

--- app/src/main/java/com/example/marcu/androidros/MainActivity.java
+++ app/src/main/java/com/example/marcu/androidros/MainActivity.java
@@ -7,15 +7,61 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EMAIL = "email";
 
     Intent intent = new Intent();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+
+
+
+
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        loginButton.setReadPermissions(Arrays.asList("public_profile"));
+        loginButton.setReadPermissions(Arrays.asList("user_photos"));
+        loginButton.setReadPermissions(Arrays.asList("user_friends"));
+
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+
 
     }
+
+
     public void nextActivityButtonClicked (View view){
         intent.setClass(this,CreateAccountActivity.class);
         startActivity(intent);
