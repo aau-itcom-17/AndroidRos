6f76f40f365fc2128df0c2ebe70baa9733cb7d32
gustavjorgensen <gjarge17@student.aau.dk>
2018-04-23 13:22:49 +0200

    Some extra work for the map. (Remember to put in your own key)

--- app/src/main/java/com/example/marcu/androidros/Map/MapActivity.java
+++ app/src/main/java/com/example/marcu/androidros/Map/MapActivity.java
@@ -119,13 +119,10 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         // Example on how to put in a customisable icon for a marker.
         mRoskilde = mMap.addMarker(new MarkerOptions()
                 .position(ROSKILDE)
-                .snippet("For more information go to www.roskilde-festival.dk/")
+                .snippet("For more information click on this window!")
                 .title("Roskilde Festival")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
         mRoskilde.setTag(0);
-
-
-
 
 
         mHome = mMap.addMarker(new MarkerOptions()
@@ -204,76 +201,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
 
-
-
-
-
-//    @Override
-//    public void onMapReady(GoogleMap map) {
-//        mMap = map;
-//        // TODO: Before enabling the My Location layer, you must request
-//        // location permission from the user. This sample does not include
-//        // a request for location permission.
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-//                    == PackageManager.PERMISSION_GRANTED) {
-//                mMap.setMyLocationEnabled(true);
-//            } else {
-//                // Show rationale and request permission.
-//            }
-//            return;
-//        }
-//        mMap.setMyLocationEnabled(true);
-//        mMap.setOnMyLocationButtonClickListener(this);
-//        mMap.setOnMyLocationClickListener(this);
-//
-//    }
-//
-//    @Override
-//    public void onMyLocationClick(@NonNull Location location) {
-//        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
-//    }
-//
-//    @Override
-//    public boolean onMyLocationButtonClick() {
-//        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
-//        // Return false so that we don't consume the event and the default behavior still occurs
-//        // (the camera animates to the user's current position).
-//        return false;
-//    }
-//
-//    @Override
-//    public void onPointerCaptureChanged(boolean hasCapture) {
-//
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-//        if (requestCode == MY_LOCATION_REQUEST_CODE) {
-//            if (permissions.length == 1 &&
-//                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
-//                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                    // TODO: Consider calling
-//                    //    ActivityCompat#requestPermissions
-//                    // here to request the missing permissions, and then overriding
-//                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//                    //                                          int[] grantResults)
-//                    // to handle the case where the user grants the permission. See the documentation
-//                    // for ActivityCompat#requestPermissions for more details.
-//                    return;
-//                }
-//                mMap.setMyLocationEnabled(true);
-//
-//            }
-//            // Permission was denied. Display an error message.
-//        }
-//
-//
-//    }
-
-
     private void setUpBottomNavigationView(){
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
@@ -305,7 +232,5 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         Intent intent = new Intent(MapActivity.this,EventPopUp.class);
         startActivity(intent);
         }
-
-
     }
 }
