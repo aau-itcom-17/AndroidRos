77e2018a8d47f0bd34e331f75d541db68c271891
bastianhoj <bastianhoejbjerre@gmail.com>
2018-04-23 14:56:39 +0200

    Cleaned up MapActivity

--- app/src/main/java/com/example/marcu/androidros/Map/MapActivity.java
+++ app/src/main/java/com/example/marcu/androidros/Map/MapActivity.java
@@ -9,29 +9,22 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.marcu.androidros.List.ListActivity;
 import com.example.marcu.androidros.R;
 import com.example.marcu.androidros.Utils.BottomNavigationViewHelper;
 import com.example.marcu.androidros.Utils.EventPopUp;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
@@ -47,8 +40,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private ActionBarDrawerToggle mToggle;
 
     private GoogleMap mMap;
-    private LocationManager locationManager;
-    private LocationListener locationListener;
     private boolean mPermissionDenied = false;
 
     private static final LatLng AAU = new LatLng(55.649114, 12.542689);
@@ -213,15 +204,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
-
-//    @Override
-//    public boolean onMarkerClick(Marker marker) {
-//        if(marker.equals(mRoskilde)) {
-//            Intent intent = new Intent(MapActivity.this,EventPopUp.class);
-//            startActivity(intent);
-//        }
-//        return false;
-//    }
 
 
     @Override
