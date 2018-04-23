153fd2a7c7eb60d42bed35d3b29b01d4377f5c2b
daugerdas <daugerdas@gmail.com>
2018-04-23 15:12:23 +0200

    Intro screen for first time users

--- app/src/main/java/com/example/marcu/androidros/Login/MainActivity.java
+++ app/src/main/java/com/example/marcu/androidros/Login/MainActivity.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,7 +12,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.marcu.androidros.Intro.Application;
+import com.example.marcu.androidros.IntroActivity;
 import com.example.marcu.androidros.R;
+import com.example.marcu.androidros.Utils.Prefs;
+
 import java.util.Arrays;
 
 
@@ -21,12 +26,21 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String EMAIL = "email";
 
+    //private Prefs prefs;
+
+    SharedPreferences prefs = null;
+
+
+
+
     Intent intent = new Intent();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        prefs = getSharedPreferences("com.example.marcu.androidros", MODE_PRIVATE);
+
+        setContentView(R.layout.activity_main);
     }
 
     public void nextActivityButtonClicked (View view){
@@ -39,5 +53,16 @@ public class MainActivity extends AppCompatActivity {
         intent.setClass(this,LoginActivity.class);
         startActivity(intent);
         Log.i("TEST", "Going to Login Page");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (prefs.getBoolean("firstrun", true)) {
+            intent.setClass(this, IntroActivity.class);
+            startActivity(intent);
+            prefs.edit().putBoolean("firstrun", false).commit();
+        }
     }
 }
