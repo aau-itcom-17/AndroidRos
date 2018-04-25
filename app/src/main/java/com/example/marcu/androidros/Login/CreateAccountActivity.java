e7f13dd806f287a2986dfb0dc864e967f941e3f6
Anton <akroghp@gmail.com>
2018-04-25 14:09:26 +0200

    CreateAccount add photo from gallery

--- app/src/main/java/com/example/marcu/androidros/Login/CreateAccountActivity.java
+++ app/src/main/java/com/example/marcu/androidros/Login/CreateAccountActivity.java
@@ -1,13 +1,22 @@
 package com.example.marcu.androidros.Login;
 
+import android.Manifest;
 import android.app.Activity;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +27,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -55,15 +66,22 @@ public class CreateAccountActivity extends AppCompatActivity {
     String email;
     String password;
     String confirmPassword;
+    String picturePath;
+
+    private static int RESULT_LOAD_IMAGE = 1;
+    ImageView imageView;
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         setHideKeyboardOnTouch(this, findViewById(R.id.activity_create_account));
-        // getting the viewmodel:
-        //viewModel = ViewModelProviders.of(this).get(CreateAccountViewModel.class);
+
+        imageView = (ImageView) findViewById(R.id.profilePictureView);
+        imageButton = (ImageButton) findViewById(R.id.addProfilePictureButton);
     }
+
 
     public void createAccountButtonClicked (View view){
         intent.setClass(this, MainActivity.class);
@@ -107,10 +125,57 @@ public class CreateAccountActivity extends AppCompatActivity {
 
             startActivity(intent);
         }
+    }
+    public void addProfilePictureButtonClicked(View view){
+        checkGalleryPermissions();
+        Intent i = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
 
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+    }
+
+    private void checkGalleryPermissions(){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        RESULT_LOAD_IMAGE);
+            }
+        } else {
+            // Permission has already been granted
+        }
     }
 
     public static boolean isValidEmail(CharSequence target) {
@@ -120,7 +185,6 @@ public class CreateAccountActivity extends AppCompatActivity {
             return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
         }
     }
-
 
     public static void setHideKeyboardOnTouch(final Context context, View view) {
         //Set up touch listener for non-text box views to hide keyboard.
