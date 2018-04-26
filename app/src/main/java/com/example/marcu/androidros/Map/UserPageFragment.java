66a458226c9480e25832655686c7b562fee8c8b0
bastianhoj <bastianhoejbjerre@gmail.com>
2018-04-26 13:04:48 +0200

    Edited navigation drawer

---
+++ app/src/main/java/com/example/marcu/androidros/Map/UserPageFragment.java
@@ -0,0 +1,19 @@
+package com.example.marcu.androidros.Map;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.marcu.androidros.R;
+
+public class UserPageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_userpage, container, false);
+    }
+}
