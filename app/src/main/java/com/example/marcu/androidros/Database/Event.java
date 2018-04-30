497d870eb11068c8b9e7f28afc8aadf61322b2df
Anton <akroghp@gmail.com>
2018-04-30 14:12:37 +0200

    Event change

--- app/src/main/java/com/example/marcu/androidros/Database/Event.java
+++ app/src/main/java/com/example/marcu/androidros/Database/Event.java
@@ -5,56 +5,47 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.PrimaryKey;
 
-@Entity (tableName = "event", foreignKeys = @ForeignKey(
-        entity = User.class,
-        childColumns = "event_id",
-        parentColumns = "user_id",
-        onDelete = ForeignKey.CASCADE)
-)
 public class Event {
 
-    public Event(String name, String description, String photoPath, String time, String date, String location) {
+    public Event(String eventID, String name, String description, String photoPath, String time, String date, String location, int likes, int comments) {
+        this.eventID = eventID;
         this.name = name;
         this.description = description;
         this.photoPath = photoPath;
         this.time = time;
         this.date = date;
         this.location = location;
+        this.likes = likes;
+        this.comments = comments;
     }
 
-    @PrimaryKey (autoGenerate = true)
-    @ColumnInfo (name = "event_id")
-    private int eventID;
+    public Event(){
 
-    @ColumnInfo (name = "name")
+    }
+
+    private String eventID;
+
     private String name;
 
-    @ColumnInfo (name = "description")
     private String description;
 
-    @ColumnInfo (name = "photo_path")
     private String photoPath;
 
-    @ColumnInfo (name = "time")
     private String time;
 
-    @ColumnInfo (name = "date")
     private String date;
 
-    @ColumnInfo (name = "location")
     private String location;
 
-    @ColumnInfo (name = "likes")
     private int likes;
 
-    @ColumnInfo (name = "comments")
     private int comments;
 
-    public int getEventID() {
+    public String getEventID() {
         return eventID;
     }
 
-    public void setEventID(int eventID) {
+    public void setEventID(String eventID) {
         this.eventID = eventID;
     }
 
