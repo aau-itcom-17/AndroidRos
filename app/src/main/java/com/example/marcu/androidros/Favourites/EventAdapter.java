4a7e363bce84a47a05d4a89e90472ed7103ec9da
lasseskjotthansen <lasseskjott@gmail.com>
2018-05-18 09:48:49 +0200

    - Created tap "My Events" on My Page. - tap "My Events", now show ALL events. Need to have user ID as a Event child, sto only show events made by the user.

---
+++ app/src/main/java/com/example/marcu/androidros/Favourites/EventAdapter.java
@@ -0,0 +1,88 @@
+package com.example.marcu.androidros.Favourites;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.marcu.androidros.Database.Event;
+import com.example.marcu.androidros.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<Event> myEvents;
+    private OnEventClickListener listener;
+
+
+    public interface OnEventClickListener{
+        void onEventClick(int position);
+    }
+
+    public void setOnEventClickListener(OnEventClickListener listener){
+        this.listener = listener;
+    }
+
+    public EventAdapter(Context context, ArrayList<Event> myEvents) {
+        this.myEvents = myEvents;
+        this.context = context;
+        //System.out.println("Constructor in EventAdapter: " + myEvents.size());
+
+    }
+
+
+    @NonNull
+    @Override
+    public EventAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.fragment_my_events ,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventAdapter.ViewHolder holder, int position) {
+        Event event = myEvents.get(position);
+
+        String imageUrl = event.getPhotoPath();
+        String title = event.getName();
+        String description = event.getDescription();
+
+        Picasso.get().load(imageUrl).into(holder.eventImage);
+        holder.eventName.setText(title);
+        holder.description.setText(description);
+    }
+
+    @Override
+    public int getItemCount() {
+        return myEvents.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView eventName;
+        public TextView description;
+        public ImageView eventImage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            eventName = (TextView) itemView.findViewById(R.id.my_events_fragment_event_title_text_view);
+            description = (TextView) itemView.findViewById(R.id.my_events_fragment_event_description_text_view);
+            eventImage = (ImageView) itemView.findViewById(R.id.my_events_fragment_image_view);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onEventClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/marcu/androidros/Favourites/FavouriteActivity.java
+++ app/src/main/java/com/example/marcu/androidros/Favourites/FavouriteActivity.java
@@ -5,39 +5,104 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.marcu.androidros.Database.Event;
+import com.example.marcu.androidros.List.NearbyFragment;
+import com.example.marcu.androidros.List.NewFragment;
+import com.example.marcu.androidros.List.TopFragment;
 import com.example.marcu.androidros.Utils.SectionsPagerAdapter;
 import com.example.marcu.androidros.R;
 import com.example.marcu.androidros.Utils.BottomNavigationViewHelper;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FavouriteActivity extends AppCompatActivity {
 
-    @Override
+    /*@Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites);
 
         setUpBottomNavigationView();
         setUpViewPager();
+    }*/
+
+
+    FirebaseDatabase database;
+    String TAG = "FavouriteActivity";
+    ArrayList<Event> myEvents = new ArrayList<>();
+    List<String> myEventIDs = new ArrayList<>();
+    MyEventsFragment myEventsFragment;
+    MyFavouritesFragment myFavouritesFragment;
+    MyInvitesFragment myInvitesFragment;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourites);
+
+        database = FirebaseDatabase.getInstance();
+        database.getReference("events").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    myEventIDs.add(String.valueOf(postSnapshot.child("eventID").getValue()));
+                    //Log.i(CLASS_TAG, "EventID " + event.getEventID());
+                }
+
+                for (int i = 0; i < myEventIDs.size(); i++){
+                    Event myEvent = dataSnapshot.child(myEventIDs.get(i)).getValue(Event.class);
+                    myEvents.add(myEvent);
+                }
+
+                for (int i = 0; i < myEventIDs.size(); i++){
+                    Log.i(TAG, myEventIDs.get(i));
+                }
+
+                Bundle bundle = new Bundle();
+                bundle.putParcelableArrayList("key", myEvents);
+                myEventsFragment = new MyEventsFragment();
+                myFavouritesFragment = new MyFavouritesFragment();
+                myInvitesFragment = new MyInvitesFragment();
+                myEventsFragment.setArguments(bundle);
+                myFavouritesFragment.setArguments(bundle);
+                myInvitesFragment.setArguments(bundle);
+
+
+                setUpBottomNavigationView();
+                setUpViewPager();
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     //This is setting up the 3 tabs at the top
     private void setUpViewPager (){
-        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new MyFavouritesFragment());
-        adapter.addFragment(new MyEventsFragment());
-        adapter.addFragment(new MyInvitesFragment());
+        SectionsPagerAdapter myAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        myAdapter.addFragment(myEventsFragment);
+        myAdapter.addFragment(new MyFavouritesFragment());
+        myAdapter.addFragment(new MyInvitesFragment());
         ViewPager viewPager = (ViewPager) findViewById(R.id.container);
-        viewPager.setAdapter(adapter);
+        viewPager.setAdapter(myAdapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
-        tabLayout.getTabAt(0).setText(R.string.title_favourites);
-        tabLayout.getTabAt(1).setText(R.string.title_my_events);
+        tabLayout.getTabAt(0).setText(R.string.title_my_events);
+        tabLayout.getTabAt(1).setText(R.string.title_my_favourites);
         tabLayout.getTabAt(2).setText(R.string.title_my_invites);
 
 
@@ -51,8 +116,5 @@ public class FavouriteActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
     }
-
-
-
 }
 
